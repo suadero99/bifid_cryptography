6ae276e860f426f1ec086163684cbcc58a2ce10f
Froylan Santos <froysantos99@gmail.com>
2022-02-21 12:49:58 -0600

    Se obtienen indices al cifrar

--- main.java
+++ main.java
@@ -5,34 +5,40 @@ import javax.print.event.PrintEvent;
 public class main{
   public static void main(String[] args) {
     //Pasamos args[0] (la llave) a un arreglo de char
-    char[] key = "ENCRYPT".toCharArray(); 
-
+    char[] key = "ENCRYPT".toCharArray();
     //Abecedario
-    String[] letters = new String[]{"A","B","C","D","E","F","G","H","I",
-      "K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+    char[] letters = new char[]{'A','B','C','D','E','F','G','H','I',
+      'K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
 
     tableauOps operador = new tableauOps(letters);
     
     //Armamos nuestra cuadrícula de 5x5
-    String[][] tableau = operador.arrangeTableau(key);
+    char[][] tableau = operador.arrangeTableau(key);
 
     operador.printTableau(tableau);
     
-    String message = args[1];
-    if(args[0] == "ENCRYPT"){
-      /****************************** 
-       * CIFRADO
-      *******************************/
-      //Pasamos args[1] (el mensaje cifrado)
-      System.out.println("Cifrado");
-      
+    //Pasamos nuestros argumentos a un solo String:
+    String message = "";
+    for(int i=1;i<args.length;i++){
+      message += args[i];
+    }
+    //Nuestro mensaje como arreglo de caracteres, para fácil manejo
+    char[] chMessage = message.toCharArray();
+    System.out.println("\nMensaje pasado a arreglo de chars\n");
+
+    if(args[0].equals("ENCRYPT")){
+      System.out.println("**************CIFRADO**************");
+      String encodedMessage = ""; //será nuestro mensaje codificado
+      int[] rows = operador.encodeIndexes(chMessage, tableau);
+
+      /*System.out.println("Indices");
+      for(int i=0;i<rows.length;i++){
+        System.out.print(rows[i]);
+      }*/
     }
 
-    if(args[0] == "DECRYPT"){
-      /******************************
-       * DECIFRADO
-       ******************************/
-      System.out.println("Decifrado");
+    if(args[0].equals("DECRYPT")){
+      System.out.println("**************DECIFRADO**************");
     }
     
 
--- tableauOps.java
+++ tableauOps.java
@@ -6,15 +6,15 @@ import java.util.List;
 
 public class tableauOps {
   int c = 0; //Contador para recorrer nuestro abecedario
-  String[] letters; //abecedario
+  char[] letters; //abecedario
 
-  public tableauOps(String[] letters){
+  public tableauOps(char[] letters){
     this.letters = letters;
   }
 
-  public String[][] arrangeTableau(char[] key){
-    String[][] tableau = new String[5][5]; //La tabla
-    List<String> usedLetters = new LinkedList<>(); //Lista de letras usadas
+  public char[][] arrangeTableau(char[] key){
+    char[][] tableau = new char[5][5]; //La tabla
+    List<Character> usedLetters = new LinkedList<>(); //Lista de letras usadas
     int keyLen = key.length; //La longitud del argumento de entrada
     boolean keyIsIn= false; //Para comprobar si los args ya se usaron para el tableau
 
@@ -39,7 +39,7 @@ public class tableauOps {
           
         }else{
           //System.out.println("i: "+i+"\nj: "+j);
-          tableau[i][j]=""+key[(i*5)+j];
+          tableau[i][j]=key[(i*5)+j];
         }
         //Añadimos a la lista de letras usadas
         usedLetters.add(tableau[i][j]);
@@ -49,7 +49,7 @@ public class tableauOps {
     return tableau;
   }
 
-  public void printTableau(String[][] tableau){
+  public void printTableau(char[][] tableau){
     //Impresión de tableau
     for(int i=0; i<5; i++){
       System.out.println();
@@ -59,4 +59,22 @@ public class tableauOps {
     }
   }
   
+  //Obtención de índices para cifrar
+  public int[] encodeIndexes(char[] chMessage, char[][] tableau){
+    int[] rows = new int[chMessage.length * 2];
+
+    for(int i=0;i<chMessage.length;i++){//Inicio busqueda de letras en arreglo
+      for(int j=0;j<5;j++){//Inicio horizontal
+        for(int k=0;k<5;k++){//Inicio vertical
+          if(tableau[j][k] == chMessage[i]){
+            //Insertamos los índices
+            rows[i]=j; //"primera" hilera
+            rows[chMessage.length+i]=k;//la "segunda" hilera
+            //System.out.print(j+""+k);
+          }
+        }//Fin vertical
+      }//Fin horizontal
+    }//Fin busqueda
+    return rows;
+  }
 }
